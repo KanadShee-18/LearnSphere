63c0a1e91fadba1a76986ed17bcc41baa08dfbae
KanadShee-18 <codewizardkanad@gmail.com>
2024-12-20 03:19:32 +0530

    Modified click functionality in searchbox

--- src/components/common/SearchBox.jsx
+++ src/components/common/SearchBox.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
 import { MdClose } from "react-icons/md";
 import { motion } from "framer-motion";
+import { LuBookMarked } from "react-icons/lu";
 
 const listitemVariants = {
   initial: { opacity: 0 },
@@ -99,11 +100,73 @@ const SearchBox = ({ setOpenBox }) => {
   }, []);
 
   return (
+    // <div
+    //   ref={searchContainerRef}
+    //   className="fixed text-slate-300 !mt-14 pt-5 grid w-screen h-screen place-items-center inset-0 z-[1000] bg-slate-950 bg-opacity-70"
+    // >
+    //   <div className="relative w-[350px] md:w-[400px] lg:w-[500px] h-auto min-h-[200px] shadow-md flex gap-x-2 flex-col shadow-slate-950 bg-gradient-to-br from-slate-800 to-slate-900 rounded-md px-4 py-4 text-sm items-start justify-center">
+    //     <span className="absolute -top-3 -right-3 text-slate-300">
+    //       <button onClick={handleCloseButtonClick}>
+    //         <MdClose className="p-1 text-2xl rounded-full bg-slate-700 hover:bg-slate-800 hover:text-slate-200 shadow-md shadow-slate-950" />
+    //       </button>
+    //     </span>
+    //     <h1 className="text-lg mb-3 font-medium text-teal-400 font-inter tracking-wide w-full">
+    //       Find your courses here
+    //     </h1>
+    //     <div className="w-full h-full p-4 flex items-center justify-start bg-gradient-to-br from-slate-700 to-slate-800 rounded-md shadow-md shadow-slate-950">
+    //       <div className=" w-[40px] text-xl flex  items-center">
+    //         <BsSearch className="relative text-teal-400" />
+    //       </div>
+    //       <input
+    //         type="text"
+    //         value={searchValue}
+    //         onChange={handleOnChange}
+    //         placeholder="Ex: ML, AI, DSA"
+    //         className="px-2 py-1 bg-transparent placeholder:text-slate-200 outline-none tracking-wide"
+    //       />
+    //     </div>
+    //     {suggestions.length > 0 && (
+    //       <motion.ul
+    //         initial={{ y: -20, opacity: 0 }}
+    //         animate={{ y: 0, opacity: 1 }}
+    //         transition={{
+    //           duration: 0.8,
+    //           type: "spring",
+    //           stiffness: "70",
+    //           damping: "20",
+    //         }}
+    //         className=" w-full px-4 py-4 backdrop-blur-md mt-3 bg-opacity-75 rounded-md bg-slate-800 text-[#cdf1ff]"
+    //       >
+    //         {suggestions.map((suggestion, index) => (
+    //           <motion.li
+    //             variants={listitemVariants}
+    //             initial="initial"
+    //             whileInView="animate"
+    //             custom={index}
+    //             key={index}
+    //             onClick={(e) => handleLinkClick(e, suggestion.courseId)}
+    //             className=" px-2 py-3 my-2 text-xs transition-all duration-200 rounded-md shadow-md md:text-sm shadow-slate-950 hover:cursor-pointer hover:text-teal-400 bg-slate-700 hover:scale-95 text-wrap"
+    //           >
+    //             {suggestion.courseName}
+    //           </motion.li>
+    //         ))}
+    //       </motion.ul>
+    //     )}
+    //   </div>
+    // </div>
     <div
-      ref={searchContainerRef}
-      className="fixed text-slate-300 !mt-14 pt-5 grid w-screen h-screen place-items-center inset-0 z-[1000] bg-slate-950 overflow-auto bg-opacity-95 backdrop-blur-lg"
+      onClick={() => {
+        setSuggestions([]);
+        setSearchValue("");
+        setOpenBox(false); // Close the search box when clicking outside
+      }}
+      className="fixed text-slate-300 !mt-14 pt-5 grid w-screen h-screen place-items-center inset-0 z-[1000] bg-slate-950 bg-opacity-70 backdrop-blur-sm"
     >
-      <div className="relative w-[350px] md:w-[400px] lg:w-[500px] h-auto min-h-[200px] shadow-md flex gap-x-2 flex-col shadow-slate-950 bg-gradient-to-br from-slate-800 to-slate-900 rounded-md px-4 py-4 text-sm items-start justify-center">
+      <div
+        ref={searchContainerRef}
+        onClick={(e) => e.stopPropagation()} // Prevent event propagation
+        className="relative w-[350px] md:w-[400px] lg:w-[500px] h-auto min-h-[200px] shadow-md flex gap-x-2 flex-col shadow-slate-950 bg-gradient-to-br from-slate-800 to-slate-900 rounded-md px-4 py-4 text-sm items-start justify-center  scrollbar-hide"
+      >
         <span className="absolute -top-3 -right-3 text-slate-300">
           <button onClick={handleCloseButtonClick}>
             <MdClose className="p-1 text-2xl rounded-full bg-slate-700 hover:bg-slate-800 hover:text-slate-200 shadow-md shadow-slate-950" />
@@ -113,7 +176,7 @@ const SearchBox = ({ setOpenBox }) => {
           Find your courses here
         </h1>
         <div className="w-full h-full p-4 flex items-center justify-start bg-gradient-to-br from-slate-700 to-slate-800 rounded-md shadow-md shadow-slate-950">
-          <div className=" w-[40px] text-xl flex  items-center">
+          <div className="w-[40px] text-xl flex items-center">
             <BsSearch className="relative text-teal-400" />
           </div>
           <input
@@ -134,58 +197,28 @@ const SearchBox = ({ setOpenBox }) => {
               stiffness: "70",
               damping: "20",
             }}
-            className=" w-full px-4 py-4 backdrop-blur-md mt-3 bg-opacity-75 rounded-md bg-slate-800 text-[#cdf1ff]"
+            className="w-full px-4 py-4 backdrop-blur-md mt-3 bg-opacity-75 rounded-md bg-slate-800 text-[#cdf1ff]"
           >
             {suggestions.map((suggestion, index) => (
               <motion.li
                 variants={listitemVariants}
                 initial="initial"
                 whileInView="animate"
-                // viewport={{
-                //   once: true,
-                // }}
                 custom={index}
                 key={index}
                 onClick={(e) => handleLinkClick(e, suggestion.courseId)}
-                className=" px-2 py-3 my-2 text-xs transition-all duration-200 rounded-md shadow-md md:text-sm shadow-slate-950 hover:cursor-pointer hover:text-teal-400 bg-slate-700 hover:scale-95 text-wrap"
+                className="px-2 py-3 my-2 text-xs transition-all duration-100 rounded-md shadow-md md:text-sm shadow-slate-950 hover:shadow-slate-700 hover:cursor-pointer hover:text-teal-400 bg-slate-700 text-wrap"
               >
-                {suggestion.courseName}
+                <span className="flex items-center gap-x-2">
+                  <p className="text-xs md:text-sm">
+                    &rarr; {suggestion.courseName}
+                  </p>
+                </span>
               </motion.li>
             ))}
           </motion.ul>
         )}
       </div>
-      {/* {suggestions.length > 0 && (
-        <motion.ul
-          initial={{ y: -20, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{
-            duration: 0.8,
-            type: "spring",
-            stiffness: "70",
-            damping: "20",
-          }}
-          className=" w-[200px] sm:w-[300px] md:w-[500px] px-4 py-4 backdrop-blur-md mt-3 bg-opacity-75 rounded-md bg-slate-800 text-[#cdf1ff]"
-        >
-          {suggestions.map((suggestion, index) => (
-            <motion.li
-              variants={listitemVariants}
-              initial="initial"
-              whileInView="animate"
-              // viewport={{
-              //   once: true,
-              // }}
-              custom={index}
-              key={index}
-              onClick={() => handleLinkClick(suggestion.courseId)}
-              // className="w-full px-2 py-1 my-2 text-xs transition-all duration-200 rounded-md shadow-md md:text-sm shadow-slate-950 hover:cursor-pointer hover:text-teal-400 bg-slate-700 hover:scale-95 text-wrap"
-              className="w-full px-2 py-1 my-2 text-xs transition-all duration-200 rounded-md shadow-md md:text-sm shadow-slate-950 hover:cursor-pointer hover:text-teal-400 bg-slate-700 hover:scale-95 text-wrap"
-            >
-              {suggestion.courseName}
-            </motion.li>
-          ))}
-        </motion.ul>
-      )} */}
     </div>
   );
 };
